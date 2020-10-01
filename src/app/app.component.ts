9a9e19659ef5009770a116384ef7bd276f0c7c60
udayaditya singh <udayaditya.singh@gmail.com>
2020-10-01 20:27:20 +0530

    bottom reached Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bottom-reached';
+  statusText: string = "not reached";
+
+  constructor(lc: NgZone) {
+     window.onscroll = () => {
+         let status = "not reached";
+         let windowHeight = "innerHeight" in window ? window.innerHeight
+             : document.documentElement.offsetHeight;
+         let body = document.body, html = document.documentElement;
+         let docHeight = Math.max(body.scrollHeight,
+             body.offsetHeight, html.clientHeight,
+             html.scrollHeight, html.offsetHeight);
+         let windowBottom = windowHeight + window.pageYOffset;
+         if (windowBottom >= docHeight) {
+           status = 'bottom reached';
+         }
+         lc.run(() => {
+           this.statusText = status;
+         });
+      };
+  }
 }
